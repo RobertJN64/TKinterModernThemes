b16517644c663874273af2b50af866d996ddff03
RobertJN <robertjnies@gmail.com>
2021-09-22 11:23:36 -0400

    trying to make readme.md file work

--- setup.py
+++ setup.py
@@ -1,14 +1,5 @@
 from setuptools import setup
 
-def getPackages():
-    out = []
-    for item in ['azure', 'forest', 'park', 'sun-valley']:
-        out.append('TKinterModernThemes/themes/' + item)
-        out.append('TKinterModernThemes/themes/' + item + '/theme')
-        out.append('TKinterModernThemes/themes/' + item + '/theme/dark')
-        out.append('TKinterModernThemes/themes/' + item + '/theme/light')
-    return out
-
 setup(
     name='TKinterModernThemes',
     url='https://github.com/RobertJN64/TKinterModernThemes',
@@ -20,9 +11,10 @@ setup(
     include_package_data=True,
     install_requires=[],
     # *strongly* suggested for sharing
-    version='1.0.7',
+    version='1.0.8',
     # The license can be anything you like
     license='MIT',
     description='A collection of modern themes with code that makes it easy to integrate into a tkinter project..',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
 )

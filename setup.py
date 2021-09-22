ee8cbcbbb192f4b06c0b3fbdc17b1431a623979a
RobertJN <robertjnies@gmail.com>
2021-09-22 11:18:58 -0400

    trying to make readme.md file work

--- setup.py
+++ setup.py
@@ -16,10 +16,11 @@ setup(
     author_email='robertjnies@gamil.com',
     # Needed to actually package something
     packages=['TKinterModernThemes'],
+    data_files=['README.md'], #because manifest is being stupid
     include_package_data=True,
     install_requires=[],
     # *strongly* suggested for sharing
-    version='1.0.6',
+    version='1.0.7',
     # The license can be anything you like
     license='MIT',
     description='A collection of modern themes with code that makes it easy to integrate into a tkinter project..',

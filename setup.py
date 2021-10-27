a698bc4888a32070295550254152d0268d006c03
RobertJN <robertjnies@gmail.com>
2021-10-26 20:54:43 -0400

    project restructure

--- setup.py
+++ setup.py
@@ -1,23 +1,4 @@
 from setuptools import setup
 
-setup(
-    name='TKinterModernThemes',
-    url='https://github.com/RobertJN64/TKinterModernThemes',
-    author='Robert Nies',
-    author_email='robertjnies@gamil.com',
-    # Needed to actually package something
-    packages=['TKinterModernThemes'],
-    data_files=['README.md'], #because manifest is being stupid
-    include_package_data=True,
-    install_requires=[],
-    extras_require={
-        'matplotlib': ['matplotlib']
-    },
-    # *strongly* suggested for sharing
-    version='1.6.1',
-    # The license can be anything you like
-    license='MIT',
-    description='A collection of modern themes with code that makes it easy to integrate into a tkinter project..',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-)
+if __name__ == "__main__":
+    setup()
